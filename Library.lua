db9a85efb01717e6609f5e4c921e3039d1a28ded
asterionnn <134950753+asterionnn@users.noreply.github.com>
2025-03-29 14:02:02 +0800

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -32,7 +32,7 @@ local Library = {
 	FontColor = Color3.fromRGB(255, 255, 255);
 	MainColor = Color3.fromRGB(28, 28, 28);
 	BackgroundColor = Color3.fromRGB(20, 20, 20);
-	AccentColor = Color3.fromRGB(255, 255, 255);
+	AccentColor = Color3.fromRGB(207, 159, 255);
 	OutlineColor = Color3.fromRGB(50, 50, 50);
 	RiskColor = Color3.fromRGB(255, 50, 50),
 
@@ -2350,8 +2350,8 @@ do
 			if Info.Multi then
 				local T = {};
 
-				for Value, Bool in next, Dropdown.Value[Value] do
-					table.insert(T, Value);
+				for Value, Bool in next, Dropdown.Value do
+					table.insert(T, Value[Value]);
 				end;
 
 				return T;
